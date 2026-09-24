30e75d2e9d60d5b6aa6c674caab358e68e7c8605
Roseab72 <alessiabianchimano@gmail.com>
2026-09-24 02:19:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const menuButton = document.getElementById("menu-button"); 
+const navLinks = document.querySelector(".nav-links"); 
+menuButton.addEventListener("click", () => { 
+    navLinks.classList.toggle("active"); });
